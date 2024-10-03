40da612836ad8845bfd1f235d63c792e7f1d0db4
Eli <95142071+melimun@users.noreply.github.com>
2024-10-03 13:57:28 -0400

    First Test Commit

---
+++ DriveSmart/DriveSmart/DriveSmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriveSmartApp.swift
+//  DriveSmart
+//
+//  Created by Eli Munoz on 2024-10-03.
+//
+
+import SwiftUI
+
+@main
+struct DriveSmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
